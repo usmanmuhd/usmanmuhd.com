630d86ddf38013530680b489eb37cfeacd4b6e1e
Muhammad Usman <me@usmanmuhd.com>
2019-06-15 07:41:52 +0530

    JS changes

--- themes/kiera/static/js/scripts.js
+++ themes/kiera/static/js/scripts.js
@@ -1,1 +1,9 @@
-// nothing here yet.
+document.addEventListener("DOMContentLoaded", () => {
+    let a = document.querySelectorAll('a');
+    const baseReg = new RegExp(window.location.origin);
+    a.forEach((node) => {
+        if (!baseReg.test(node.href)) {
+            node.target = '_blank';
+        }
+    });
+});
